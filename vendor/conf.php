035bff8c69d1fc98859dd8a6f334f42ef8de0bdc
MIS3 <MIS3@DESKTOP-CATAOJ4>
2018-08-12 10:33:34 +0700

    cmom

--- vendor/conf.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$access_token="YkKWSyuWj6iN8OhVHNmVg8KqEGASRhdLa8flQI/UTPNtPUCnairRdBB8SHXlZ2nAdR4FMDzj8qcL/KfXYMjbJbHN2o5E3JJbzUhz2AuTKDaStyDiVk0owOiBkscGv555mqpk+u9av8p/fld9tDTl3gdB04t89/1O/w1cDnyilFU=" ;
-$channelSecret="3417c7f09002fe116a5e1fe81673c509" ;
