9f8a66348d754c8c4a12f2e3b87c0e74d0a501a5
MIS3 <MIS3@DESKTOP-CATAOJ4>
2018-08-12 12:03:44 +0700

    edit

---
+++ webhooks.php
@@ -0,0 +1,47 @@
+<?php 
+
+if($_GET['c'] == 'test')
+{
+	$url = "https://newproject.pnru.ac.th/apibotline/record.php";   
+	
+	$content = json_encode(array('conten'=>'test')) ;
+	
+	$headers = array('Content-Type: application/json');
+
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $content);  
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	//print $result ;
+	 
+}else
+{
+	echo "none" ;
+}
+
+$content = file_get_contents('php://input');  
+ 
+if ($content) {
+	
+	$url = "https://newproject.pnru.ac.th/apibotline/record.php";   
+	
+	$headers = array('Content-Type: application/json');
+
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $content);  
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	 
+	
+}
+
